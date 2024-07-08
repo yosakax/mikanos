2227f1a85828f44e4112f6293e1d89cc09a06a1d
yosaka <fallingfluit@gmail.com>
2024-07-08 23:53:04 +0900

    write white to screen by bootloader

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,4 +1,5 @@
 #include <Guid/FileInfo.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/PrintLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
@@ -119,6 +120,43 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
   return EFI_SUCCESS;
 }
 
+EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
+                   EFI_GRAPHICS_OUTPUT_PROTOCOL **gop) {
+  UINTN num_gop_handles = 0;
+  EFI_HANDLE *gop_handles = NULL;
+  gBS->LocateHandleBuffer(ByProtocol, &gEfiGraphicsOutputProtocolGuid, NULL,
+                          &num_gop_handles, &gop_handles);
+  gBS->OpenProtocol(gop_handles[0], &gEfiGraphicsOutputProtocolGuid,
+                    (VOID **)gop, image_handle, NULL,
+                    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  FreePool(gop_handles);
+
+  return EFI_SUCCESS;
+}
+
+const CHAR16 *GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
+  switch (fmt) {
+  case PixelRedGreenBlueReserved8BitPerColor:
+    return L"PixelRegGreenBlueReversed8BitPerColor";
+
+  case PixelBlueGreenRedReserved8BitPerColor:
+    return L"PixelBlueGreenRedReserved8BitPerColor";
+
+  case PixelBitMask:
+    return L"PixelBitMask";
+
+  case PixelBltOnly:
+    return L"PixelBltOnly";
+
+  case PixelFormatMax:
+    return L"PixelFormatMax";
+
+  default:
+    return L"InvalidPixelFormat";
+  }
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
                            EFI_SYSTEM_TABLE *system_table) {
   Print(L"Hello Mikan World!\n");
@@ -137,6 +175,24 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
 
   SaveMemoryMap(&memmap, memmap_file);
   memmap_file->Close(memmap_file);
+
+  // gop
+  // gop->Graphics Output Protocol
+  EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
+  OpenGOP(image_handle, &gop);
+
+  Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
+        gop->Mode->Info->HorizontalResolution,
+        gop->Mode->Info->VerticalResolution,
+        GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+        gop->Mode->Info->PixelsPerScanLine);
+
+  UINT8 *frame_buffer = (UINT8 *)gop->Mode->FrameBufferBase;
+
+  for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i) {
+    frame_buffer[i] = 255;
+  }
+  // gop
 
   // read_kernel
   EFI_FILE_PROTOCOL *kernel_file;

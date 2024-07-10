24b176cb130a23cc72f42bfff4d2888dceddee52
yosaka <fallingfluit@gmail.com>
2024-07-10 23:45:35 +0900

    add Makefile

---
+++ kernel/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size) {
+  // reinterpret_cast<T>はただの(T)val と同じ型のキャストだが，
+  // 整数からポインタへの型変換であることを明示することができてよい
+  uint8_t *frame_buffer = reinterpret_cast<uint8_t *>(frame_buffer_base);
+
+  for (uint64_t i = 0; i < frame_buffer_size; ++i) {
+    frame_buffer[i] = i % 256;
+  }
+  while (1) {
+    __asm__("hlt");
+  }
+}

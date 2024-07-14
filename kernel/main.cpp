ad2120d4995c5098adca21983c5d2ab2c796771f
yosaka <fallingfluit@gmail.com>
2024-07-14 10:17:10 +0900

    write ascii by pixel_writer

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -2,6 +2,27 @@
 #include <cstddef>
 #include <cstdint>
 
+// font a
+const uint8_t kFontA[16] = {
+    0b00000000, //
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b01111110, //  ******
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b11100111, // ***  ***
+    0b00000000, //
+    0b00000000, //
+};
+// font a
+
 struct PixelColor {
   uint8_t r, g, b;
 };
@@ -48,6 +69,22 @@ public:
 };
 // derived_pixel_writer
 
+// write_ascii
+void WriteAscii(PixelWriter &writer, int x, int y, char c,
+                const PixelColor &color) {
+  if (c != 'A') {
+    return;
+  }
+  for (int dy = 0; dy < 16; ++dy) {
+    for (int dx = 0; dx < 8; ++dx) {
+      if ((kFontA[dy] << dx) & 0x80u) {
+        writer.Write(x + dx, y + dy, color);
+      }
+    }
+  }
+}
+// write_ascii
+
 // placement_new
 void *operator new(size_t size, void *buf) { return buf; }
 
@@ -81,6 +118,10 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
+  // write aa
+  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+  // write aa
 
   while (1) {
     __asm__("hlt");
